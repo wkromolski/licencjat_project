b2fd58a7b17dc7773867d6dccd4e379b1563a4d1
Sigma <jskolska@edu.cdv.pl>
2025-04-01 18:55:10 +0200

    fixed loading screen

--- Assets/Scripts/LoadingScreenManager.cs
+++ Assets/Scripts/LoadingScreenManager.cs
@@ -2,21 +2,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
+using System.Collections;
 
 public class LoadingScreenManager : MonoBehaviour
 {
     public static LoadingScreenManager Instance;
-    
+
     [SerializeField] private GameObject loadingPanel;
     [SerializeField] private RectTransform image1;
 
-    [Header("Animation Settings")]
-    [SerializeField] private float fadeOutDuration = 1f;  
-    [SerializeField] private float loadingDelay = 2f;   
-    [SerializeField] private float fadeInDuration = 1f;  
-    
+    [Header("Animation Settings")] [SerializeField]
+    private float fadeOutDuration = 1f;
+
+    [SerializeField] private float loadingDelay = 2f;
+    [SerializeField] private float fadeInDuration = 1f;
+
     private Sequence loadingSequence;
-    
+
     private Image image1Image;
 
     private void Awake()
@@ -33,24 +35,16 @@ public class LoadingScreenManager : MonoBehaviour
         }
     }
 
-    private void OnEnable()
-    {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-    }
-    private void OnDisable()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
     private void Start()
     {
         if (loadingPanel != null)
             loadingPanel.SetActive(false);
-        
+
         if (image1 != null)
         {
             image1.localScale = Vector3.one * 1.5f;
             image1Image = image1.GetComponent<Image>();
+
             if (image1Image != null)
             {
                 Color col = image1Image.color;
@@ -63,56 +57,49 @@ public class LoadingScreenManager : MonoBehaviour
             }
         }
     }
-    
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        PlayFadeIn();
-    }
-    
+
     public void LoadSceneWithTransition(string sceneName)
     {
-        if (loadingPanel != null)
-            loadingPanel.SetActive(true);
-
-        loadingSequence = DOTween.Sequence();
-        
-        if (image1 != null)
-        {
-            loadingSequence.Append(image1.DOScale(1f, fadeOutDuration).SetEase(Ease.OutQuad));
-            if (image1Image != null)
-                loadingSequence.Join(image1Image.DOFade(1f, fadeOutDuration));
-        }
-        
-        loadingSequence.AppendInterval(loadingDelay);
-        
-        loadingSequence.AppendCallback(() =>
-        {
-            SceneManager.LoadScene(sceneName);
-        });
-
-        loadingSequence.Play();
+        StartCoroutine(LoadSceneWithImmediateActivation(sceneName));
     }
-    
-    public void PlayFadeIn()
+
+    private IEnumerator LoadSceneWithImmediateActivation(string sceneName)
     {
         if (loadingPanel != null)
             loadingPanel.SetActive(true);
 
-        Sequence fadeInSequence = DOTween.Sequence();
-
         if (image1 != null)
         {
-            fadeInSequence.Append(image1.DOScale(1.5f, fadeInDuration).SetEase(Ease.InQuad));
+            image1.localScale = Vector3.one * 1.5f;
             if (image1Image != null)
-                fadeInSequence.Join(image1Image.DOFade(0f, fadeInDuration));
+            {
+                Color col = image1Image.color;
+                col.a = 0f;
+                image1Image.color = col;
+            }
         }
-
-        fadeInSequence.OnComplete(() =>
+        
+        Sequence fadeOut = DOTween.Sequence();
+        fadeOut.Append(image1.DOScale(1f, fadeOutDuration).SetEase(Ease.OutQuad));
+        fadeOut.Join(image1Image.DOFade(1f, fadeOutDuration));
+        yield return fadeOut.WaitForCompletion();
+        
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        asyncLoad.allowSceneActivation = true;
+        
+        while (!asyncLoad.isDone)
         {
-            if (loadingPanel != null)
-                loadingPanel.SetActive(false);
-        });
+            yield return null;
+        }
+        
+        yield return new WaitForSeconds(loadingDelay);
+        
+        Sequence fadeIn = DOTween.Sequence();
+        fadeIn.Append(image1.DOScale(1.5f, fadeInDuration).SetEase(Ease.InQuad));
+        fadeIn.Join(image1Image.DOFade(0f, fadeInDuration));
+        yield return fadeIn.WaitForCompletion();
 
-        fadeInSequence.Play();
+        if (loadingPanel != null)
+            loadingPanel.SetActive(false);
     }
 }
